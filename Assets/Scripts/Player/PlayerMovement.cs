0a574a9fd83e1728fa63143bb78cd004628a1141
SeanOBrien2702 <seanobrien2702@gmail.com>
2025-02-17 13:33:15 -0500

    Added player movement and dashing

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashCooldown;
+    [SerializeField] float dashDuration;
+    Vector2 direction = Vector2.zero;
+    Vector2 lastDirection = Vector2.zero;
+    float dashTimer;
+    Vector3 left = new Vector3(-1, 1, 1);
+    Rigidbody2D body;
+
+    bool isDashing = false;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        
+        if (direction != Vector2.zero &&
+            !isDashing)
+        {
+            lastDirection = direction;
+        }
+        Dash();
+        LookAtDirection();
+    }
+
+    void FixedUpdate()
+    {
+        if(isDashing)
+        {
+            Debug.Log("dashing " + lastDirection + " " + direction);
+            body.linearVelocity = lastDirection.normalized * dashSpeed ;
+        }
+        else
+        {
+            body.linearVelocity = direction.normalized * moveSpeed;
+        }         
+    }
+
+    void LookAtDirection()
+    {
+        if (direction != lastDirection) return;
+        if (lastDirection.x >= 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else
+        {
+            transform.localScale = left;
+        }
+    }
+
+    void Dash()
+    {
+        dashTimer += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) &&
+           dashTimer > dashCooldown)
+        {
+            dashTimer = 0;
+            isDashing = true;
+            Invoke("StopDash", dashDuration);
+            
+        }    
+    }
+
+    void StopDash()
+    {
+        isDashing = false;
+    }
+}
